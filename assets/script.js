3bb4033036de3eace7bf14c29b95144a056e3d4c
Higor De'Setti <61642318+settih@users.noreply.github.com>
2023-01-11 19:04:37 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const hour = document.getElementById('hour');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    hour.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = s;
+})
